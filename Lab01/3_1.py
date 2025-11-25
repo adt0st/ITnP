fc28c4ff16c5d0a0b85aca73cb333a404c23474d
adt0st <59542030+adt0st@users.noreply.github.com>
2025-11-25 18:29:13 +0300

    Create 3_1.py

---
+++ Lab01/3_1.py
@@ -0,0 +1,9 @@
+def read_file(read_type=0):
+    with open('example1.txt', 'r', encoding='utf8') as file:
+        if not read_type:
+            print('Весь файл:\n' + file.read())
+        else:
+            print('Строки:')
+            for line in file:
+                print(line)
+read_file(int(input("Выберите тип чтения:\n0 (пустое значение) - весь файл\n1 - построчно\n") or 0))
