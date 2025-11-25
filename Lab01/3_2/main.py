09c0a1edc02cf54738696352cfb3b41eaf402ef8
adt0st <59542030+adt0st@users.noreply.github.com>
2025-11-25 19:59:01 +0300

    Update main.py

--- Lab01/3_2/main.py
+++ Lab01/3_2/main.py
@@ -1,4 +1,4 @@
 def add_text(text=''):
     with open('user_input.txt', 'a+', encoding='utf8') as file:
         file.write(text)
-add_text(input('Введите текст для записи в файл:\n'))3
+add_text(input('Введите текст для записи в файл:\n'))
