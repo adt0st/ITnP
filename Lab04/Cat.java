47dc2d287bfa5d70d5102a846d928aa9e0b913ae
adt0st <59542030+adt0st@users.noreply.github.com>
2025-12-02 20:43:56 +0300

    Update Cat.java

--- Lab04/Cat.java
+++ Lab04/Cat.java
@@ -1,14 +1,42 @@
-public class Cat extends Animal {
-    public Cat(String name, int age) {
-        super(name, age);
+class Cat extends Animal {
+    private String breed;
+    private boolean isIndoor;
+
+    public Cat() {
+        super();
+        this.breed = "Unknown";
+        this.isIndoor = false;
+    }
+
+    public Cat(String name, int age, String habitat, String breed, boolean isIndoor) {
+        super(name, age, habitat);
+        this.breed = breed;
+        this.isIndoor = isIndoor;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public boolean isIndoor() {
+        return isIndoor;
+    }
+
+    public void setIndoor(boolean indoor) {
+        isIndoor = indoor;
     }
 
     @Override
     public void makeSound() {
-        System.out.println(name + " мяукает.");
+        System.out.println(name + " говорит: Мяу");
     }
 
-    public void feed() {
-        System.out.println(name + " кормится.");
+    @Override
+    public void move() {
+        System.out.println(name + " ходит изящно.");
     }
 }
