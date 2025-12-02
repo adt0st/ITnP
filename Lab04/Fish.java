c86edd3ff82b67583aac9b84ee96965f5dc32cac
adt0st <59542030+adt0st@users.noreply.github.com>
2025-12-02 20:47:15 +0300

    Update Fish.java

--- Lab04/Fish.java
+++ Lab04/Fish.java
@@ -1,9 +1,42 @@
-public class Fish extends Animal {
-    public Fish(String name, int age) {
-        super(name, age);
+class Fish extends Animal {
+    private String species;
+    private boolean isFreshwater;
+
+    public Fish() {
+        super();
+        this.species = "Unknown";
+        this.isFreshwater = true;
     }
-  
-    public void swim() {
+
+    public Fish(String name, int age, String habitat, String species, boolean isFreshwater) {
+        super(name, age, habitat);
+        this.species = species;
+        this.isFreshwater = isFreshwater;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public boolean isFreshwater() {
+        return isFreshwater;
+    }
+
+    public void setFreshwater(boolean freshwater) {
+        isFreshwater = freshwater;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " булькает.");
+    }
+
+    @Override
+    public void move() {
         System.out.println(name + " плывёт.");
     }
 }
