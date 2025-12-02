8b3ce04f5b9dd829006184785b1a3ebade0e095e
adt0st <59542030+adt0st@users.noreply.github.com>
2025-12-02 19:49:44 +0300

    Update Primes.java

--- Lab03/Primes.java
+++ Lab03/Primes.java
@@ -1,9 +1,10 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
 public class Primes {
     public static void main(String[] args) {
+        for (int i = 2; i < 100; i++) {
+          if (isPrime(i) == true) {
+              System.out.println(i);
+          }
+    }
     }  
     
     public static boolean isPrime(int n) {  
