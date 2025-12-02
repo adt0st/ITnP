575151a9e4ea464d508d837dda2dc60664d8b1c5
adt0st <59542030+adt0st@users.noreply.github.com>
2025-12-02 20:46:37 +0300

    Update Parrot.java

--- Lab04/Parrot.java
+++ Lab04/Parrot.java
@@ -1,14 +1,49 @@
-public class Parrot extends Animal {
-    public Parrot(String name, int age) {
-        super(name, age);
+class Parrot extends Animal {
+    private String color;
+    private boolean canTalk;
+    private static int count = 0;
+
+    public Parrot() {
+        super();
+        this.color = "Green";
+        this.canTalk = false;
+        count++;
+    }
+
+    public Parrot(String name, int age, String habitat, String color, boolean canTalk) {
+        super(name, age, habitat);
+        this.color = color;
+        this.canTalk = canTalk;
+        count++;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isCanTalk() {
+        return canTalk;
+    }
+
+    public void setCanTalk(boolean canTalk) {
+        this.canTalk = canTalk;
+    }
+
+    public static int getCount() {
+        return count;
     }
 
     @Override
     public void makeSound() {
-        System.out.println(name + " чирикает.");
+        System.out.println(name + " говорит: Чирик");
     }
 
-    public void fly() {
+    @Override
+    public void move() {
         System.out.println(name + " летает.");
     }
 }
