e96277a68a0e2e7c2a963afc3c94289c0874acdb
adt0st <59542030+adt0st@users.noreply.github.com>
2025-12-02 19:30:37 +0300

    Create Primes.java

---
+++ Lab03/Primes.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Primes {
+    public static void main(String[] args) {
+    }  
+    
+    public static boolean isPrime(int n) {  
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    } 
+}
